4ad3ef66cb4a34dff5cd0921503009232ac3ec41
akshaaatt <tiwariakshat03@gmail.com>
2022-03-06 03:54:40 +0530

    Swagger docs added

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from qiskit import QuantumCircuit, BasicAer, execute
 
 
 def run_qasm(qasm, backend_to_run='qasm_simulator', num_shots_str='1'):
-    """Comments"""
     circuit = QuantumCircuit.from_qasm_str(qasm)
     backend = BasicAer.get_backend(backend_to_run)
     job_sim = execute(circuit, backend, shots=int(num_shots_str))
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 """Imports"""
+from flasgger import Swagger
 from flask import Flask
 
 from routes import configure_routes
 
 app = Flask(__name__)
-
+swagger = Swagger(app)
 configure_routes(app)
 
 if __name__ == '__main__':
