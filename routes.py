0a050a9c3a2c5a488e3e70e8db950778520ad355
akshaaatt <tiwariakshat03@gmail.com>
2022-04-09 23:37:28 +0530

    Cleanup code

--- routes.py
+++ routes.py
@@ -1,20 +1,21 @@
 """Imports"""
-from flask import jsonify, request
-from api import run_qasm
+from flask import jsonify
 import qrng
 
 
 def configure_routes(app):
     """Routes"""
+
     @app.route('/')
     def welcome():
         """Welcome Users"""
         return "Hi Qiskitter!"
 
-
     @app.route('/api/key', methods=['GET', 'POST'])
     def qrng_key():
         """Comments"""
-        qrng.set_provider_as_IBMQ('57fcd139a2ca2e77337f1f5dc2a5a39a2aa0c862981fcb33a2469bc63f47a22a3c8df9f074157f984ee96896ffeb3886f44a137a0b967922f05b91b4ae494a6b')
+        qrng.set_provider_as_IBMQ(
+            '57fcd139a2ca2e77337f1f5dc2a5a39a2aa0c862981fcb33a2469bc63f47a22a3c8df9f074157f984ee96896ffeb3886f44a137a0b967922f05b91b4ae494a6b'
+        )
         qrng.set_backend('ibmq_mumbai')
         return jsonify(qrng.get_random_int64())
